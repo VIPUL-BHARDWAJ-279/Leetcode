6bbaadd22955f34e6c4c333e8d6777d4035fc2a7
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-18 13:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (50.01%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int tri(int n,vector<int> & dp){
-        if(n==0)return dp[0]=0;
-        if(n==1 || n==2)return dp[1]=1;
-        if(dp[n]!=-1)return dp[n];
-        else return dp[n]=tri(n-1,dp)+tri(n-2,dp)+tri(n-3,dp);
-    }
-    int tribonacci(int n){
-        vector<int> dp(n+1,-1);
-        return tri(n,dp);
-    }
-    // Iterative Code
+    // int tri(int n,vector<int> & dp){
+    //     if(n==0)return dp[0]=0;
+    //     if(n==1 || n==2)return dp[1]=1;
+    //     if(dp[n]!=-1)return dp[n];
+    //     else return dp[n]=tri(n-1,dp)+tri(n-2,dp)+tri(n-3,dp);
+    // }
+    // int tribonacci(int n){
+    //     vector<int> dp(n+1,-1);
+    //     return tri(n,dp);
+    // }
+    // // Iterative Code
     // int tribonacci(int n) {
     //     long long t0=0,t1=1,t2=1;
     //     for(int i=0;i<n;i++){
@@ -21,4 +21,17 @@ public:
     //     }
     //     return int(t0);
     // }
+
+    int tribonacci(int n){
+        long long t0=0,t1=1,t2=1,ans;
+        if(n==0)return t0;
+        if(n==1 || n==2)return 1;
+        for(int i=2;i<n;i++){
+            ans=t2+t1+t0;
+            t0=t1;
+            t1=t2;
+            t2=ans;
+        }
+        return ans;
+    }
 };

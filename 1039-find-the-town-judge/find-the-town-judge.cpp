ae3e4e86a7f871a932959a9fc5f3f56f61845471
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-18 19:59:45 +0530

    Time: 156 ms (15.06%) | Memory: 77.2 MB (8.73%) - LeetSync

---
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<vector<int>> adj(n+1);
+        for(auto x:trust)adj[x[0]].push_back(x[1]);
+        int cnt=0,ind=-1;
+        for(int i=1;i<n+1;i++)if(adj[i].size()==0)cnt++,ind=i;
+        if(cnt==1){
+            int k=0;
+            for(int i=1;i<n+1;i++){
+                if(i!=ind){
+                    for(int j=0;j<adj[i].size();j++)if(adj[i][j]==ind)k++;
+                }
+            }
+            if(k==n-1)return ind;
+            return -1;
+        }
+        return -1;
+    }
+};

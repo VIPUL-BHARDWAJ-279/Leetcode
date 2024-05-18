42a8faeffaa1cf19913a0de7ddac4c9c3d150e9b
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-18 20:05:45 +0530

    Time: 128 ms (52.54%) | Memory: 73.1 MB (18.40%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -2,25 +2,12 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-        vector<vector<int>> adj(n+1);
-        for(auto x:trust)adj[x[0]].push_back(x[1]);
-        int cnt=0,ind=-1;
-        for(int i=1;i<n+1;i++)if(adj[i].size()==0)cnt++,ind=i;
-        if(cnt==1){
-            int k=0;
-            for(int i=1;i<n+1;i++){
-                if(i!=ind){
-                    for(int j=0;j<adj[i].size();j++){
-                        if(adj[i][j]==ind){
-                            k++;
-                            break;
-                        }   
-                    }
-                }
-            }
-            if(k==n-1)return ind;
-            return -1;
+        int trustOfNode[1001]={0};
+        for(auto x:trust){
+            trustOfNode[x[0]]--;
+            trustOfNode[x[1]]++;
         }
-        return -1;
+        for(int i=1;i<1001;i++)if(trustOfNode[i]==n-1)return i;
+        return -1;  
     }
 };

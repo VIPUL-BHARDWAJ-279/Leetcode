9b70ea48f90bc9c263d3245ee8cc0e93f69c2bd5
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-19 09:50:51 +0530

    Time: 9 ms (63.64%) | Memory: 16.3 MB (51.24%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.cpp
+++ 740-delete-and-earn/delete-and-earn.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
 
     int deleteAndEarn(vector<int>& nums) {
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
         vector<int> bucket(10001);
         for(auto num:nums)bucket[num]+=num;
         int prev1=0,prev2=0;

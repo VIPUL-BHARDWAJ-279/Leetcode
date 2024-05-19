9cf187ae25b7ed9800ea7c393bc82a363b2f6c84
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-19 19:38:17 +0530

    Time: 17 ms (29.23%) | Memory: 17.8 MB (22.18%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -2,24 +2,23 @@ class Solution {
 public:
     void dfs(int root, vector<bool> & vis, vector<vector<int>> & adj, vector<int> temp, vector<vector<int>> & ans){
         if(root==adj.size()-1){
+            temp.push_back(root);
             ans.push_back(temp);
             return;
         }
         vis[root]=true;
-        
+        temp.push_back(root);
         for(auto it:adj[root]){
-            temp.push_back(it);
             if(!vis[it])dfs(it,vis,adj,temp,ans);
-            temp.pop_back();
         }
-        vis[root]=false;
-        
+        temp.pop_back();
+        vis[root]=false; 
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& adj) {
         vector<vector<int>> ans;
         vector<bool> vis(adj.size(),false);
         vector<int> temp;
-        temp.push_back(0);
+        // temp.push_back(0);
         dfs(0,vis,adj,temp,ans);
         return ans;
     }

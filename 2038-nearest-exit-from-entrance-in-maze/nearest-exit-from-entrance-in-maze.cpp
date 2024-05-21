9500815aa53f605aa0a3a85f50b845f693e0bc94
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-21 10:55:10 +0530

    Time: 128 ms (29.98%) | Memory: 41.9 MB (29.46%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<int>> direction={{1,0},{0,1},{-1,0},{0,-1}};
+    bool isBoundaryPoint(int i,int j, int m, int n){
+        return (i==0 || i==m-1 || j==0 || j==n-1);
+    }
+    bool isEntrance(int i, int j, vector<int> & root){
+        if(i==root[0] && j==root[1])return true;
+        return false;
+    }
+    int nearestExit(vector<vector<char>>& maze, vector<int>& root) {
+        int m=maze.size(),n=maze[0].size();
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        queue<vector<int>> q;
+        int steps=0;
+        vis[root[0]][root[1]]=true;
+        q.push({root[0],root[1],steps});
+        while(!q.empty()){
+            int i=q.front()[0],j=q.front()[1];
+            steps=q.front()[2];
+            q.pop();
+            if(isBoundaryPoint(i,j,m,n) && !isEntrance(i,j,root)){
+                return steps;
+            }
+
+            for(int k=0;k<4;k++){
+                int newi=i+direction[k][0];
+                int newj=j+direction[k][1];
+                if( newi>=0 && newi<m && newj>=0 && newj<n && vis[newi][newj]==false){
+                    if(maze[newi][newj]=='.'){
+                        vis[newi][newj]=true;
+                        q.push({newi,newj,steps+1});
+                    }
+                }
+            }
+        }
+
+        return -1;
+
+    }
+};

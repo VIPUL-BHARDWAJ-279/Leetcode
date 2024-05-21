221fa8e42f0b32856fe7892715139ac73cea22f3
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-21 08:26:49 +0530

    Time: 2 ms (53.70%) | Memory: 11 MB (38.74%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(vector<vector<int>> &v, vector<int> &temp, vector<int> &nums, int i){
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        if(i==nums.size()){
+            v.push_back(temp);
+            return;
+        }
+
+        solve(v,temp,nums,i+1);
+
+        temp.push_back(nums[i]);
+        solve(v,temp,nums,i+1);
+        temp.pop_back();
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> v;
+        vector<int> temp;
+        solve(v,temp,nums,0);
+        return v;
+    }
+};

0ef97a7cd1093b23dfbfd98912315123c42bff5e
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-23 03:00:02 +0530

    Time: 424 ms (19.88%) | Memory: 289.9 MB (14.10%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
-    bool isPalindrome(string & s, int start, int end){
-        while(start<end)if(s[start++]!=s[end--])return false;
-        return true;
+    int isPalindrome(string & str, int s, int e,vector<vector<int>> & dp){
+        if(s>=e)return dp[s][e]=1;
+        if(dp[s][e]!=-1)return dp[s][e];
+        if(str[s]==str[e])return dp[s][e]=isPalindrome(str,s+1,e-1,dp);
+        else return dp[s][e]=0;
     }
     string longestPalindrome(string s) {
-        if(s.size()==1)return s;
-        string ans="";
-        int maxi=INT_MIN;
+        int n=s.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+
+        int maxi=INT_MIN,sp=-1; //sp=starting point
         for(int i=0;i<s.size();i++){
             for(int j=0;j<s.size();j++){
-                if(isPalindrome(s,i,j) && j-i+1>maxi){
-                    ans=s.substr(i,j-i+1);
-                    maxi=j-i+1;
+                if(isPalindrome(s,i,j,dp)==1){  
+                    int len=j-i+1; 
+                    if(len>maxi){
+                        maxi=len;
+                        sp=i;
+                    }
                 }
             }
         }
+        string ans=s.substr(sp,maxi);
         return ans;
     }
 };

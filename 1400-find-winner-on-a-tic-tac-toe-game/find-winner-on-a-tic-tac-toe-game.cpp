71a55e1bef452e9723864c817188b7dfae7563be
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-23 19:17:40 +0530

    Time: 4 ms (33.22%) | Memory: 9.8 MB (48.74%) - LeetSync

--- 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.cpp
+++ 1400-find-winner-on-a-tic-tac-toe-game/find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    string gameStatus(vector<vector<char>>& board){
-        char winchar=' ';
-        for(int i=0;i<3;i++){
-            if(board[i][0]==board[i][1] && board[i][1]==board[i][2]){
-                if(board[i][0]=='X')return "A";
-                else if(board[i][0]=='O')return "B";
-                else return "Pending";
-            }
-        }
-        for(int j=0;j<3;j++){
-            if(board[0][j]==board[1][j] && board[1][j]==board[2][j]){
-                if(board[0][j]=='X')return "A";
-                else if(board[0][j]=='O')return "B";
-                else return "Pending";
-            }
-        }
-        if((board[0][0]==board[1][1] && board[1][1]==board[2][2]) || (board[0][2]==board[1][1] && board[1][1]==board[2][0])){
-            if(board[1][1]=='X')return "A";
-            else if(board[1][1]=='O')return "B";
-            else return "Pending";
-        }
-        for(int i=0;i<3;i++)for(int j=0;j<3;j++)if(board[i][j]==' ')return "Pending";
-        return "Draw";
-    }
     string tictactoe(vector<vector<int>>& moves) {
-        vector<vector<char>> board(3,vector<char>(3,' '));
-        bool flag=true;
-        for(auto move:moves){
-            int x=move[0],y=move[1];
-            board[x][y]=flag?'X':'O';
-            flag=!flag;
+        int n=3;
+        int rows[3]={0},cols[3]={0};
+        int mainDiag=0,antiDiag=0;
+
+        int player=1;
+        for(int i=0;i<moves.size();i++){
+            int r=moves[i][0],c=moves[i][1];
+            rows[r]+=player;
+            cols[c]+=player;
+            if(r==c)mainDiag+=player;
+            if(r+c==n-1)antiDiag+=player;
+            if(abs(rows[r])==n || abs(cols[c])==n || abs(mainDiag)==n || abs(antiDiag)==n)return player==1?"A":"B";
+            player=-player;
         }
-        return gameStatus(board);
+        return moves.size()==9?"Draw":"Pending";
     }
 };

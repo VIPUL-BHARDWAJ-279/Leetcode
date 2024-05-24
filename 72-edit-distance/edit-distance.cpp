02642d25b61012450121fad3674f25b0a48a9a79
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-24 14:33:31 +0530

    Time: 13 ms (28.81%) | Memory: 12 MB (33.57%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int f(int i, int j, string & s1, string & s2, vector<vector<int>> & dp){
-        if(i==0)return j+1;
-        if(j==0)return i+1;
+        if(i<0)return j+1;
+        if(j<0)return i+1;
         if(dp[i][j]!=-1)return dp[i][j];
 
-        if(s1[i-1]==s2[j-1])return dp[i][j]=f(i-1,j-1,s1,s2,dp);
+        if(s1[i]==s2[j])return dp[i][j]=f(i-1,j-1,s1,s2,dp);
 
         int replace=1+f(i-1,j-1,s1,s2,dp);
         int insert=1+f(i,j-1,s1,s2,dp);
@@ -14,15 +14,36 @@ public:
         return dp[i][j]=min(replace,min(insert,del));
 
     }
-    // int minDistByTabulation(string & word1, string & word2){
-    //     int n=word1.size(),m=word2.size();
-    //     vector<vector<int>>dp(n,vector<int>(m,-1));
-
-    // }
-    int minDistance(string word1, string word2) {
+    int minDistByTabulation(string & word1, string & word2){
         int n=word1.size(),m=word2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        
-        return f(n,m,word1,word2,dp)-1;
+
+        for(int i=0;i<n+1;i++)dp[i][0]=i+1;
+        for(int j=0;j<m+1;j++)dp[0][j]=j+1;
+
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(word1[i-1]==word2[j-1])dp[i][j]=dp[i-1][j-1];
+
+                else{
+                    int replace=1+dp[i-1][j-1];
+                    int insert=1+dp[i][j-1];
+                    int del=1+dp[i-1][j];
+
+                    dp[i][j]=min(replace,min(insert,del));
+                }
+            }
+        }
+        return dp[n][m]-1;
+    }
+
+    
+    int minDistance(string word1, string word2) {
+        // if(word1=="" && word2=="")return 0;
+        // int n=word1.size(),m=word2.size();
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return f(n,m,word1,word2,dp);
+        return minDistByTabulation(word1,word2);
+
     }
 };

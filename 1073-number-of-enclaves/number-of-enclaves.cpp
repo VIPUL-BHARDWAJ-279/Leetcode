3dcfae937aac62ef3d0f9a07f9592df297ae2207
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-25 18:13:19 +0530

    Time: 192 ms (13.22%) | Memory: 46.6 MB (14.24%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<pair<int,int>> directions={{0,1},{1,0},{-1,0},{0,-1}};
-    bool onBoundary(int i, int j, int & m, int & n){
-        return (i==0 || j==0 || i==m-1 || j==n-1);
-    }
-    bool inBoundary(int & i, int & j, int & m, int & n){
-        return (i>=0 && j>=0 && i<=m-1 && j<=n-1);
-    }
-    void dfs(int i,int j,int & m,int & n, vector<vector<bool>> & vis ,vector<vector<int>> & grid){
+    void dfs(int i, int j, vector<vector<bool>> & vis , vector<vector<int>> & grid){
         vis[i][j]=true;
         for(auto & [dx,dy]:directions){
             int nx=i+dx,ny=j+dy;
-            if(inBoundary(nx,ny,m,n) && grid[nx][ny]==1 && !vis[nx][ny])dfs(nx,ny,m,n,vis,grid);
+            if(nx>=0 && ny>=0 && nx<grid.size() && ny<grid[0].size() && grid[nx][ny]==1 && !vis[nx][ny])dfs(nx,ny,vis,grid);
         }
     }
     int numEnclaves(vector<vector<int>>& grid) {
         ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+
         int m=grid.size(),n=grid[0].size();
         vector<vector<bool>> vis(m,vector<bool>(n,false));
         int ans=0;
         for(int i=0;i<m;i++){
-            if(onBoundary(i,0,m,n) && grid[i][0]==1 && !vis[i][0])dfs(i,0,m,n,vis,grid);
-            if(onBoundary(i,n-1,m,n) && grid[i][n-1]==1 && !vis[i][n-1])dfs(i,n-1,m,n,vis,grid);   
+            if(grid[i][0]==1)dfs(i,0,vis,grid);
+            if(grid[i][n-1]==1)dfs(i,n-1,vis,grid);   
         }
         for(int j=0;j<n;j++){
-            if(onBoundary(0,j,m,n) && grid[0][j]==1 && !vis[0][j])dfs(0,j,m,n,vis,grid);
-            if(onBoundary(m-1,j,m,n) && grid[m-1][j]==1 && !vis[m-1][j])dfs(m-1,j,m,n,vis,grid);
+            if(grid[0][j]==1)dfs(0,j,vis,grid);
+            if(grid[m-1][j]==1)dfs(m-1,j,vis,grid);
         }
         for(int i=0;i<m;i++)for(int j=0;j<n;j++)if(grid[i][j]==1 && !vis[i][j])ans++;
         return ans;

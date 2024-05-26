fef94bc47fcacbf013c67785beb4231e5df89569
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-26 17:52:09 +0530

    Time: 259 ms (24.47%) | Memory: 294.5 MB (22.33%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,18 +1,35 @@
 class Solution {
 public:
-    int lis(int ind, int top, vector<int> & a, vector<vector<int>> & dp){ //top == previous taken index
-        if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
-        if(dp[ind][top+1]!=-1)return dp[ind][top+1];
-        int take=0;
-        if(top==-1 || a[ind]>a[top])take= 1+lis(ind+1,ind,a,dp);
-        int notTake= 0+lis(ind+1,top,a,dp);
-        return dp[ind][top+1]= max(take,notTake);
-    }
+    // int lis(int ind, int top, vector<int> & a, vector<vector<int>> & dp){ //top == previous taken index
+    //     if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
+    //     if(dp[ind][top+1]!=-1)return dp[ind][top+1];
+    //     int take=0;
+    //     if(top==-1 || a[ind]>a[top])take= 1+lis(ind+1,ind,a,dp);
+    //     int notTake= 0+lis(ind+1,top,a,dp);
+    //     return dp[ind][top+1]= max(take,notTake);
+    // }
 
+
+    // int lengthOfLIS(vector<int>& a){
+    //     vector<vector<int>> dp(a.size(),vector<int>(a.size()+1,-1));
+    //     return lis(0,-1,a,dp);
+    // }
 
     int lengthOfLIS(vector<int>& a){
-        vector<vector<int>> dp(a.size(),vector<int>(a.size()+1,-1));
-        return lis(0,-1,a,dp);
+        int n=a.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+
+        for(int j=0;j<n+1;j++)dp[n][j]=0;
+
+        for(int ind=n-1;ind>=0;ind--){
+            for(int top=ind-1;top>=-1;top--){
+                int take=0;
+                if(top==-1 || a[ind]>a[top])take=1+dp[ind+1][ind+1];
+                int notTake=dp[ind+1][top+1];
+                dp[ind][top+1]=max(take,notTake);
+            }
+        }
+        return dp[0][-1+1];
     }
     // int lengthOfLIS(vector<int>& a) {
         // int n=a.size();

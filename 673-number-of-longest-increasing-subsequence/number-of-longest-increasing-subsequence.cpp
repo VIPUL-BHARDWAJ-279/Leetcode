1bc531d46639b566637249e06bfea7f39c754135
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-26 22:21:37 +0530

    Time: 43 ms (37.45%) | Memory: 15.7 MB (52.21%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
 
     int findNumberOfLIS(vector<int>& a) {
-        int n = a.size();
+        ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL);
+        int n = a.size(),maxLen=1,ans=0;
         vector<int> lisLen(n,1),count(n,1); // lisLen[i] will give length of lis ending at index i  & count[i] will give Count of lis till that index
 
-        int maxLen=1;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 if(a[j]<a[i]){
-                    if(lisLen[i]==lisLen[j]+1)count[i]+=count[j];
-                    else if(lisLen[i]<lisLen[j]+1){
+                    if(lisLen[i]==lisLen[j]+1)count[i] += count[j];
+                    else if(lisLen[j]+1 > lisLen[i]){
                         count[i]=count[j];
-                        lisLen[i]=max(lisLen[i],lisLen[j]+1);
+                        lisLen[i]=lisLen[j]+1;
                     }
                 }
             }
             maxLen=max(maxLen,lisLen[i]);
         }
-        int ans=0;
+
         for(int i=0;i<n;i++)if(lisLen[i]==maxLen)ans+=count[i];
         return ans;
     }

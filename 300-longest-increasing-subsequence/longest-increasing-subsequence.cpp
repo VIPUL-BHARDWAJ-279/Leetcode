23baefaad636b041540f2249f4097c496b88b405
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-26 17:40:19 +0530

    Time: 586 ms (5.04%) | Memory: 294.8 MB (9.49%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,13 +1,28 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& a) {
-        int n=a.size(),maxLen=1;
-        vector<int> ans;
-        for(int i=0;i<n;i++){
-            int ind=lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
-            if(ind==ans.size())ans.push_back(a[i]);
-            else ans[ind]=a[i];
-        }
-        return ans.size();
+    int lis(int ind, int top, vector<int> & a, vector<vector<int>> & dp){ //top == previous taken index
+        if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
+        if(dp[ind][top+1]!=-1)return dp[ind][top+1];
+        int take=0;
+        if(top==-1 || a[ind]>a[top])take= 1+lis(ind+1,ind,a,dp);
+        int notTake= 0+lis(ind+1,top,a,dp);
+        return dp[ind][top+1]= max(take,notTake);
     }
+
+
+    int lengthOfLIS(vector<int>& a){
+        vector<vector<int>> dp(a.size(),vector<int>(a.size()+1,-1));
+        return lis(0,-1,a,dp);
+    }
+    // int lengthOfLIS(vector<int>& a) {
+        // int n=a.size();
+        // vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     int ind=lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
+        //     if(ind==ans.size())ans.push_back(a[i]);
+        //     else ans[ind]=a[i];
+        // }
+        // return ans.size();
+
+    // }
 };

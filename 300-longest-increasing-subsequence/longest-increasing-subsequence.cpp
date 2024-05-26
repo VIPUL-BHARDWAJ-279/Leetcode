c685f8d4e51f60a386e850bfba2c0e73ddefd743
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-26 21:26:40 +0530

    Time: 81 ms (49.73%) | Memory: 12.8 MB (62.12%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -3,10 +3,9 @@ public:
     // int lis(int ind, int top, vector<int> & a, vector<vector<int>> & dp){ //top == previous taken index
     //     if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
     //     if(dp[ind][top+1]!=-1)return dp[ind][top+1];
-    //     int take=0;
-    //     if(top==-1 || a[ind]>a[top])take= 1+lis(ind+1,ind,a,dp);
-    //     int notTake= 0+lis(ind+1,top,a,dp);
-    //     return dp[ind][top+1]= max(take,notTake);
+    //     int len= 0+lis(ind+1,top,a,dp);
+    //     if(top==-1 || a[ind]>a[top])len=max(len,1+lis(ind+1,ind,a,dp));
+    //     return dp[ind][top+1]= len;
     // }
 
 
@@ -14,6 +13,9 @@ public:
     //     vector<vector<int>> dp(a.size(),vector<int>(a.size()+1,-1));
     //     return lis(0,-1,a,dp);
     // }
+
+
+
 
     // int lengthOfLIS(vector<int>& a){ //tabulation
     //     int n=a.size();
@@ -24,33 +26,60 @@ public:
 
     //     for(int ind=n-1;ind>=0;ind--){
     //         for(int top=ind-1;top>=-1;top--){
-    //             int take=0;
-    //             if(top==-1 || a[ind]>a[top])take=1+dp[ind+1][ind+1];
-    //             int notTake=dp[ind+1][top+1];
-    //             dp[ind][top+1]=max(take,notTake);
+    //             int len=dp[ind+1][top+1]
+    //             if(top==-1 || a[ind]>a[top])len=max(len,1+dp[ind+1][ind+1]);
+    //             dp[ind][top+1]=len;
     //         }
     //     }
     //     return dp[0][-1+1];
     // }
 
-    int lengthOfLIS(vector<int>& a){ //tabulation + space optimization
-        int n=a.size();
-        vector<int> curr(n+1,0),nexti(n+1,0); // here we have done coordinate shifting for 'top' as it ranges from -1 to n-1
-                                                        // therefore to make it from 0 to n, we always add '1' to everything written in second dimension of dp
 
 
-        for(int ind=n-1;ind>=0;ind--){
-            for(int top=ind-1;top>=-1;top--){
-                int take=0;
-                if(top==-1 || a[ind]>a[top])take=1+nexti[ind+1];
-                int notTake=nexti[top+1];
-                curr[top+1]=max(take,notTake);
+
+
+    // int lengthOfLIS(vector<int>& a){ //tabulation + space optimization
+    //     int n=a.size();
+    //     vector<int> curr(n+1,0),nexti(n+1,0); // here we have done coordinate shifting for 'top' as it ranges from -1 to n-1
+    //                                                     // therefore to make it from 0 to n, we always add '1' to everything written in second dimension of dp
+
+
+    //     for(int ind=n-1;ind>=0;ind--){
+    //         for(int top=ind-1;top>=-1;top--){
+
+    //             int len=nexti[top+1];
+    //             if(top==-1 || a[ind]>a[top])len=max(len,1+nexti[ind+1]);
+    //             curr[top+1]=len;
+    //         }
+    //         nexti=curr;
+    //     }
+    //     return nexti[-1+1];
+    // }
+
+
+
+
+
+    int lengthOfLIS(vector<int>& a){
+        int n = a.size();
+        vector<int> lisLen(n,1); // lisLen[i] will give length of lis from index 0 till index i
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(a[j]<a[i]){
+                    lisLen[i]=max(lisLen[i],lisLen[j]+1);
+                }
             }
-            nexti=curr;
         }
-        return nexti[-1+1];
+        int maxLen=1;
+        for(int i=0;i<n;i++)maxLen=max(maxLen,lisLen[i]);
+        return maxLen;
     }
-    // int lengthOfLIS(vector<int>& a) {
+
+
+
+
+    // int lengthOfLIS(vector<int>& a) { //using lower bound inbuilt function (binary search)
         // int n=a.size();
         // vector<int> ans;
         // for(int i=0;i<n;i++){

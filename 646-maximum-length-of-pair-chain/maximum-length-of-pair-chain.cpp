a7fb70faea8bb35f544508c44a2ce377263a07ec
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-27 15:31:21 +0530

    Time: 46 ms (58.31%) | Memory: 25.9 MB (93.69%) - LeetSync

--- 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/maximum-length-of-pair-chain.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
 
-    int lis(int ind, int top, vector<vector<int>> & a, vector<vector<int>> & dp){ //top == previous taken index
-        if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
-        if(dp[ind][top+1]!=-1)return dp[ind][top+1];
-        int len= 0+lis(ind+1,top,a,dp);
-        if(top==-1 || a[ind][0]>a[top][1])len=max(len,1+lis(ind+1,ind,a,dp));
-        return dp[ind][top+1]= len;
+    // int lis(int ind, int top, vector<vector<int>> & a, vector<vector<int>> & dp){ //top == previous taken index
+    //     if(ind==a.size())return 0; // here we are returning the size of the lis from index=ind to index=a.size()-1
+    //     if(dp[ind][top+1]!=-1)return dp[ind][top+1];
+    //     int len= 0+lis(ind+1,top,a,dp);
+    //     if(top==-1 || a[ind][0]>a[top][1])len=max(len,1+lis(ind+1,ind,a,dp));
+    //     return dp[ind][top+1]= len;
+    // }
+    // int findLongestChain(vector<vector<int>>& pairs) {
+    //     sort(pairs.begin(),pairs.end());
+    //     vector<vector<int>> dp(pairs.size(),vector<int>(pairs.size()+1,-1));
+    //     return lis(0,-1,pairs,dp);
+    // }
+    static bool compi(vector<int> & a, vector<int> & b){
+        return a[1]<b[1];
     }
-    int findLongestChain(vector<vector<int>>& pairs) {
-        sort(pairs.begin(),pairs.end());
-        vector<vector<int>> dp(pairs.size(),vector<int>(pairs.size()+1,-1));
-        return lis(0,-1,pairs,dp);
+    int findLongestChain(vector<vector<int>>& pairs){
+        sort(pairs.begin(),pairs.end(),compi);
+        int ans=1,prevEnd=pairs[0][1];
+        for(int i=1;i<pairs.size();i++){
+            int currStart=pairs[i][0];
+            if(prevEnd<currStart)prevEnd=pairs[i][1],ans++;
+        }
+        return ans;
+
     }
 };

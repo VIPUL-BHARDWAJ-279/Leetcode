27acfd7a65331dd34280377ef321729ede4cd2b1
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-28 16:37:27 +0530

    Time: 47 ms (98.86%) | Memory: 85.7 MB (86.96%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bill) {
+        ios_base::sync_with_stdio(false),cin.tie(NULL);
+        
         int n=bill.size(),c5=0,c10=0;
         for(int i=0;i<n;i++){
             if(bill[i]==5)c5++;
             else if(bill[i]==10){
                 c10++;
-                if(c5==0)return false;
-                else c5--;
+                if(c5)c5--;
+                else return false;
             }
             else if(bill[i]==20){
-                if(c10==0){
-                    if(c5<3)return false;
-                    else c5-=3;
-                }
-                else {
-                    c10--;
-                    if(c5==0)return false;
-                    else c5--;
-                }
+                if(c10 && c5)c10--,c5--;
+                else if( c10==0 && c5>=3)c5-=3;
+                else return false;
             }
         }
         return true;

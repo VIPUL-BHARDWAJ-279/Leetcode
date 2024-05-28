4e4a457310b088f35e94408a9bd4f02ade24ef3d
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-28 16:42:46 +0530

    Time: 15 ms (99.05%) | Memory: 25.3 MB (68.14%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -7,7 +7,7 @@ public:
         sort(a.begin(),a.end());
         for(int i=n-3;i>=0;i--){
             int x=a[i],y=a[i+1],z=a[i+2];
-            if(x+y>z && y+z>x && x+z>y)return x+y+z;
+            if(x+y>z)return x+y+z;
         }
         return 0;
     }

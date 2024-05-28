df04d034a0db4c0a24a769f77ca5a1744a12a239
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-28 07:44:37 +0530

    Time: 165 ms (98.75%) | Memory: 97.1 MB (98.20%) - LeetSync

--- 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
+++ 1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        int maxi=0;
-        for(int i=0;i<edges.size();i++){
-            maxi=max(maxi,edges[i][0]);
-            maxi=max(maxi,edges[i][1]);
-        }
-        vector<bool>isReachable(maxi+1,false);
+        ios_base::sync_with_stdio(false),cin.tie(NULL);
+
+        vector<bool>isReachable(n,false);
         vector<int> ans;
 
         for(int i=0;i<edges.size();i++)isReachable[edges[i][1]]=true;

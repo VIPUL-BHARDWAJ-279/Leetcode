5e9549f783e5db5e5da68e657168cfd4d2f08c11
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-30 01:34:28 +0530

    Time: 6 ms (37.52%) | Memory: 10.1 MB (15.29%) - LeetSync

---
+++ 38-count-and-say/count-and-say.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+    string RLE(string & s){
+        int n=s.size(),cnt=0;
+        char ch=s[0];
+        string ans="";
+
+        for(auto x:s){
+            if(x==ch)cnt++;
+            else{
+                ans+=to_string(cnt) + ch;
+                ch=x;
+                cnt=1;
+            }
+        }
+        ans+=to_string(cnt);
+        ans+=ch;
+
+        return ans;
+    }
+
+    string CandS(int n, vector<string> & dp){
+        if(n==1)return "1";
+        if(dp[n]!="")return dp[n];
+        string temp=CandS(n-1,dp);
+        return dp[n]=RLE(temp);
+    }
+
+    string countAndSay(int n) {
+        vector<string> dp(31,"");
+        return CandS(n,dp);
+    }
+};

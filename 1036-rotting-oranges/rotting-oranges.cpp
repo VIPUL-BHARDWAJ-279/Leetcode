338294005672ded9081c5014c48866736cf8aceb
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-29 10:01:24 +0530

    Time: 11 ms (12.06%) | Memory: 15.8 MB (65.34%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m=grid.size(),n=grid[0].size();
+        vector<pair<int,int>> directions={{0,1},{1,0},{-1,0},{0,-1}};
+        queue<pair<int,int>> q;
+        int level=0;
+        bool haveFresh=false;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2)q.push({i,j});
+                else if(grid[i][j]==1)haveFresh=true;
+            }
+        }
+        if(!haveFresh)return 0;
+        
+        while(!q.empty()){
+            int siz=q.size();
+            while(siz--){
+                auto [x,y]=q.front();q.pop();
+                
+                for(auto [dx,dy]:directions){
+                    int nx=x+dx,ny=y+dy;
+                    if(nx>=0 && ny>=0 && nx<m && ny<n && grid[nx][ny]==1)grid[nx][ny]=2,q.push({nx,ny});
+                }
+            }
+            level++;
+        }
+
+        for(int i=0;i<m;i++)for(int j=0;j<n;j++)if(grid[i][j]==1)return -1;
+
+        return level-1;
+    }
+};

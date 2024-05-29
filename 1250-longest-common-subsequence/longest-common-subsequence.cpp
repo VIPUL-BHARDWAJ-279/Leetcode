060ea1c3261ff42310b10349a6ae782942f7d3b0
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-05-29 13:25:39 +0530

    Time: 57 ms (5.17%) | Memory: 26 MB (8.30%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,10 +3,10 @@ public:
     int solve(int  i,int  j, string & s , string & t, vector<vector<int>> & dp){
         if(i==s.size() || j==t.size())return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        int take=0,notTake=0;
+        int take=0,cantTake=0;
         if(s[i]==t[j])take=1+solve(i+1,j+1,s,t,dp);
-        if(s[i]!=t[j]) notTake=max(solve(i+1,j,s,t,dp),solve(i,j+1,s,t,dp));
-        return dp[i][j]=max(take,notTake);
+        if(s[i]!=t[j]) cantTake=max(solve(i+1,j,s,t,dp),solve(i,j+1,s,t,dp));
+        return dp[i][j]=max(take,cantTake);
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size(),m=text2.size();

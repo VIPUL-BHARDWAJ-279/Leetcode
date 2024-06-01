eb723e87b1fbd1d5267062a743772936fe1fb340
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-06-01 14:34:15 +0530

    Time: 190 ms (56.05%) | Memory: 137.6 MB (52.44%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+    // int solve( int day, int tran , vector<int>& prices, vector<vector<int>> & dp){
+    //     if(day==prices.size()  ||  tran ==2*2)return 0;
+    //     if(dp[day][tran]!=-1) return dp[day][tran];
+    //     if(tran % 2 == 0){ // Buy
+    //         return dp[day][tran]=max(-prices[day]+solve(day+1,tran+1, prices,dp), solve(day+1,tran,prices,dp)); // tran = number of transactions done
+    //     }
+    //     else return dp[day][tran]=max(prices[day] + solve(day+1,tran+1,prices,dp),solve(day+1,tran,prices,dp));
+    // }
+    // int maxProfit(vector<int>& prices) {
+    //     int n=prices.size();
+    //     vector<vector<int>> dp(n,vector<int>(4,-1));
+    //     return solve(0,0,prices,dp);
+    // }
 
-    int dp[100001][2][2];
-    int solve(int day,int tDone, bool canBuy,vector<int> & prices){
-        if(day==prices.size() || tDone==2)return 0;
-
-        if(dp[day][canBuy][tDone]!=-1)return dp[day][canBuy][tDone];
-
-        if(canBuy){
-            int buy= -prices[day] + solve(day+1, tDone, !canBuy, prices);
-            int notBuy= solve(day+1, tDone, canBuy, prices);
-            return dp[day][canBuy][tDone]=max(buy,notBuy);
-        }
-        else{
-            int sell= prices[day] + solve(day+1, tDone+1, !canBuy, prices);
-            int notSell= solve(day+1, tDone, canBuy, prices);
-            return dp[day][canBuy][tDone]=max(sell,notSell);
-        }
-    }
     int maxProfit(vector<int>& prices) {
-        memset(dp,-1,sizeof(dp));
-        int transactionsDone=0;
-        return solve(0,transactionsDone,true,prices);
+        int n=prices.size();
+        int k=2;
+        vector<vector<int>> dp(n+1,vector<int>(2*k+1,0));
+
+        for(int day=n-1;day>=0;day--){
+            for(int tran=2*k-1;tran>=0;tran--){
+                if(tran % 2 == 0) dp[day][tran]=max(-prices[day] + dp[day+1][tran+1], dp[day+1][tran]);
+                else dp[day][tran]=max(prices[day] + dp[day+1][tran+1], dp[day+1][tran]);
+            }
+        }
+        return dp[0][0];
+        
     }
 };

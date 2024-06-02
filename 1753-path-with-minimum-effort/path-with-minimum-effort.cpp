99e6ea599cdaa8cc0ac34754f07fc4f130c6ec69
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-06-02 22:39:37 +0530

    Time: 472 ms (5.04%) | Memory: 135.3 MB (5.00%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int n=heights.size(),m=heights[0].size();
+        set<vector<int>> seti;
+        vector<vector<int>> effort(n,vector<int>(m,1e9));
+
+        vector<pair<int,int>> directions = {{0,1},{1,0},{-1,0},{0,-1}};
+
+        seti.insert({0,0,0});  // {x,y,currEffort calculated from initial point to x,y}
+        effort[0][0]=0;
+
+        while(!seti.empty()){
+            vector<int> curr = *(seti.begin());     
+            int x=curr[0], y=curr[1], maxEffortTillNow=curr[2];
+            if(x==n-1 && y==m-1)return maxEffortTillNow;
+            seti.erase(curr);
+            for(auto [dx,dy]:directions){
+                int nx=x+dx, ny=y+dy;
+                if(nx>=0 && ny>=0 && nx<n && ny<m){
+                    int newEffort=max(maxEffortTillNow,abs(heights[nx][ny]-heights[x][y]));
+                    if(newEffort < effort[nx][ny]){
+                        if(effort[nx][ny] != 1e9)seti.erase({nx,ny,effort[nx][ny]});
+                        
+                        effort[nx][ny]=newEffort;
+                        seti.insert({nx,ny,newEffort});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};

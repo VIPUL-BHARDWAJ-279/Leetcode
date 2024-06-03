8056fc5bbe642c6fbafc3d292378a96c4d3ef376
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-06-03 10:57:40 +0530

    Time: 8 ms (98.27%) | Memory: 12 MB (11.40%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
+        ios_base::sync_with_stdio(false),cin.tie(NULL);
         int x=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==t[x])x++;
-        }
+        for(int i=0;i<s.size();i++)if(s[i]==t[x])x++;
         return t.size()-x;
     }
 };

64b39955c732aa8fa9507adbd8c71950e8a800de
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-06-04 14:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (26.04%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         unordered_map<char,int> mp;
-        int totalEven=0,totalOdd=0,oddi=0;
+        int ans=0,oddi=0;
         for(int i=0;i<s.size();i++)mp[s[i]]++;
         for(auto x:mp){
-            if(x.second%2==0)totalEven+=x.second;
-            else totalOdd+=x.second-1,oddi=1;;
+            if(x.second%2==0)ans+=x.second;
+            else ans+=x.second-1,oddi=1;;
         }
-        return totalEven+totalOdd+oddi;
+        return ans+oddi;
     }
 };

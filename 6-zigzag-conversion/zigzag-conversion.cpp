f9800d55f419a3894a68d62d2c4c506b1c3a81c5
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-06-18 20:36:43 +0530

    Time: 25 ms (20.90%) | Memory: 16.5 MB (19.68%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string convert(string s, int rows) {
+        int n=s.size();
+        if(rows==1)return s;
+        vector<vector<char>> adj(rows);
+        bool uptodown=true;
+        adj[0].push_back(s[0]);
+        int k=1;
+        while(k<n){
+            if(uptodown==true){
+                for(int i=1;i<rows;i++){
+                    adj[i].push_back(s[k]);
+                    k++;
+                    if(k>=n)break;
+                }
+                uptodown=false;
+            }
+            else{
+                for(int i=rows-2;i>=0;i--){
+                    adj[i].push_back(s[k]);
+                    k++;
+                    if(k>=n)break;
+                }
+                uptodown=true;
+            }
+        }
+        string ans="";
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<adj[i].size();j++)ans+=adj[i][j];
+        }
+        return ans;
+        
+    }
+};

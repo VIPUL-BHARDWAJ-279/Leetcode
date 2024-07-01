7b51a3063377cc55a24d95ce8b4dfd7833fb0f8c
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-01 20:16:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (45.76%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& a) {
+        int n=a.size();
+        if(n<=2)return false;
+        for(int i=0;i<=n-3;i++)if(a[i]%2 && a[i+1]%2 && a[i+2]%2)return true;
+        return false;
+    }
+};

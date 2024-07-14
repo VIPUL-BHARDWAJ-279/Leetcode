f876b717c717940699ccba4c37f4e2f9cb12a1fc
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-14 15:36:14 +0530

    Time: 3 ms (87.34%) | Memory: 11.2 MB (75.75%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,22 +1,64 @@
 #include<bits/stdc++.h>
 class Solution {
 public:
+    // int dp[301];
+
+    // bool solve(int i, string & s, unordered_set<string> & bank){
+    //     if(i==s.size())return dp[i]=1;
+    //     if(dp[i]!=-1)return dp[i];
+    //     string temp="";
+    //     for(int j=i;j<s.size();j++){
+    //         temp+=s[j];
+    //         if(bank.find(temp)!=bank.end())if(solve(j+1,s,bank))return dp[i]=1;
+    //     }
+    //     return dp[i]=0;
+    // }
+    // bool wordBreak(string s, vector<string>& wordDict) {
+    //     memset(dp,-1,sizeof(dp));
+    //     unordered_set<string> bank(wordDict.begin(),wordDict.end());
+    //     return solve(0,s,bank)==1?true:false;
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     int dp[301];
 
-    bool solve(int i, string & s, unordered_set<string> & bank){
+    bool solve(int i,string & s, unordered_set<string> & bank){
         if(i==s.size())return dp[i]=1;
         if(dp[i]!=-1)return dp[i];
         string temp="";
-        for(int j=i;j<s.size();j++){
-            temp+=s[j];
-            if(bank.find(temp)!=bank.end())if(solve(j+1,s,bank))return dp[i]=1;
+        for(int k=i;k<s.size();k++){
+            temp+=s[k];
+            if(bank.find(temp)!=bank.end()){
+                if(solve(k+1,s,bank)==true)return dp[i]=1;
+            }
         }
         return dp[i]=0;
-        
     }
     bool wordBreak(string s, vector<string>& wordDict) {
         memset(dp,-1,sizeof(dp));
-        unordered_set<string> bank(wordDict.begin(),wordDict.end());
-        return solve(0,s,bank)==1?true:false;
+        unordered_set<string>bank(wordDict.begin(),wordDict.end());
+        return solve(0,s,bank);
     }
 };

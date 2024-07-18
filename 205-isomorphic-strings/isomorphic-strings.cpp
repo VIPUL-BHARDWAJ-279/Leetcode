e1503c8ba95547c927ad1ee2170db9a972c93e0c
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-18 16:14:06 +0530

    Time: 4 ms (76.43%) | Memory: 9.7 MB (8.06%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -22,8 +22,8 @@ public:
 
         for(int i=0;i<s.size();i++){
             if(mps[s[i]]!=mpt[t[i]])return false;
-            mps[s[i]]=i+123;
-            mpt[t[i]]=i+123;
+            mps[s[i]]=i+1;
+            mpt[t[i]]=i+1;
         }
 
         return true;

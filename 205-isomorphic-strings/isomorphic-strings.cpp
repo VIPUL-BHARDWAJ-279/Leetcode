fb2c38626e6e480a47b515ecddbf90c94fa36e0b
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-18 16:13:33 +0530

    Time: 8 ms (36.21%) | Memory: 9.7 MB (8.06%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    // bool isIsomorphic(string s, string t) {
+    //     ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    //     if(s.size()!=t.size())return false;
+    //     unordered_map<char,char> m1,m2;
+    //     for(int i=0;i<s.size();i++){
+    //         if(!m1[s[i]] && !m2[t[i]]){
+    //             m1[s[i]]=t[i];
+    //             m2[t[i]]=s[i];
+    //         }
+    //         else if(m1[s[i]]!=t[i])return false;
+    //     } 
+    //     return true;
+    // }
+
+
+    bool isIsomorphic(string s, string t){
+        ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+        unordered_map<char,int> mps,mpt;
+        if(s.size()!=t.size())return false;
+
+        for(int i=0;i<s.size();i++){
+            if(mps[s[i]]!=mpt[t[i]])return false;
+            mps[s[i]]=i+1234;
+            mpt[t[i]]=i+1234;
+        }
+
+        return true;
+    }
+};

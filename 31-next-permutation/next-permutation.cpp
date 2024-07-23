73e40002402ec33edc60d86f9b5ab848434c3cb2
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-23 13:31:33 +0530

    Time: 4 ms (55.75%) | Memory: 14.9 MB (9.66%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -27,7 +27,7 @@ public:
 
 
     void nextPermutation(vector<int>& a) {
-        int n=a.size(),dipPoint=-1;
+        int n=a.size(),dipPoint=-1; // next permutation banane ke liye koi ek number badhana hoga (uske aage aane wale numbers me se kisi se swap karke) aur uske aage wale numbers ko sort karna hoga. DipPoint aisa first number leta hai backside se taaki aage se longest matching ho sake.
         for(int i=n-2;i>=0;i--){
             if(a[i]<a[i+1]){
                 dipPoint=i;

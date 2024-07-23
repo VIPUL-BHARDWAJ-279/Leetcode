6222de47732857f8518bdaf57a1793b177f33b08
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-07-23 12:26:44 +0530

    Time: 4 ms (33.28%) | Memory: 8.2 MB (18.65%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -6,8 +6,8 @@ public:
         for(int i=1;i<n;i++){
             vector<int>temp(i+1);
             temp[0]=1;
+            for(int j=1;j<=i-1;j++)temp[j]=ans[i-1][j-1]+ans[i-1][j];
             temp[i]=1;
-            for(int j=1;j<i;j++)temp[j]=ans[i-1][j-1]+ans[i-1][j];
             ans.push_back(temp);
         }
 

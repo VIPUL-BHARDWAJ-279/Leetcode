75f7278bd62f049590799b220a23f2f07af0dbf8
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-08-01 13:10:53 +0530

    Time: 8 ms (47.01%) | Memory: 17.5 MB (27.35%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.cpp
@@ -3,8 +3,8 @@ public:
     int countSeniors(vector<string>& details) {
         int ans=0;
         for(int i=0;i<details.size();i++){
-            int age=stoi(details[i].substr(11,2));
-            if(age>60)ans++;
+            string age=(details[i].substr(11,2));
+            if(age>"60")ans++;
         }
         return ans;
     }

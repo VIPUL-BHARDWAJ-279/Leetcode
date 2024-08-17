a5793495cd8f318b9ae9045f662301bc46d3bf91
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-08-17 19:31:24 +0530

    Time: 217 ms (35.15%) | Memory: 129 MB (36.78%) - LeetSync

--- 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/maximum-number-of-points-with-cost.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     long long maxPoints(vector<vector<int>>& points) {
-        int row = points.size();
-        int col = points[0].size();
-        vector<long long> dp(col, 0);
-        
-        for(int i = 0; i < col; i++) {
-            dp[i] = points[0][i];
+        int m=points.size(),n=points[0].size();
+        vector<long long> prev(n);
+        for(int col=0;col<n;col++)prev[col]=points[0][col];
+
+        for(int row=1;row<m;row++){
+            vector<long long> left(n),right(n),curr(n);
+
+            left[0]=prev[0];
+            for(int col=1;col<n;col++){
+                left[col]=max(prev[col],left[col-1]-1);
+            }
+
+            right[n-1]=prev[n-1];
+            for(int col=n-2;col>=0;col--){
+                right[col]=max(prev[col],right[col+1]-1);
+            }
+
+            for(int col=0;col<n;col++)curr[col]=points[row][col]+max(left[col],right[col]);
+
+            prev=curr;
+
         }
-        
-        for(int r = 1; r < row; r++) {
-            vector<long long> leftMax(col, 0), rightMax(col, 0);
-            vector<long long> newDp(col, 0);
-            
-            leftMax[0] = dp[0];
-            for(int i = 1; i < col; i++) {
-                leftMax[i] = max(leftMax[i-1], dp[i] + i);
-            }
-            
-            rightMax[col-1] = dp[col-1] - (col-1);
-            for(int i = col-2; i >= 0; i--) {
-                rightMax[i] = max(rightMax[i+1], dp[i] - i);
-            }
-            
-            for(int i = 0; i < col; i++) {
-                newDp[i] = max(leftMax[i] - i, rightMax[i] + i) + points[r][i];
-            }
-            
-            dp = newDp;
-        }
-        
-        return *max_element(dp.begin(), dp.end());
+
+        return *max_element(prev.begin(),prev.end());
     }
 };

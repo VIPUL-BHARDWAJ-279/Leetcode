09302b127073b1135ae1ea625dc772abda98e394
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-08-24 12:22:06 +0530

    Time: 12 ms (63.47%) | Memory: 12.1 MB (51.64%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.size(),maxLen=0,l=0,r=0;
         while(r<n){
             if(mp.find(s[r])!=mp.end()){
-                if(mp[s[r]]>=l)l=mp[s[r]]+1;
+                if(l<=mp[s[r]])l=mp[s[r]]+1;
             }
             maxLen=max(maxLen,r-l+1);
             mp[s[r]]=r;

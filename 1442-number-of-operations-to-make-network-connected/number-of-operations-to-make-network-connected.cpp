68fc29a7fedcc5a4f2b7d1081ca12e0a90c5de97
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-10-23 18:46:09 +0530

    Time: 56 ms (95.54%) | Memory: 54.3 MB (18.23%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,52 +1,33 @@
-class DisjointSet {
-public:
-    vector<int> parent,rank,size;
-    DisjointSet(int n){
-        rank.resize(n+1,0);
-        size.resize(n+1,1);
-
-        parent.resize(n+1);
-        for(int i=0;i<n+1;i++)parent[i]=i;
-    }
-
-    int findPar(int node){ // Find Ultimate Parent of Node
-        if(node==parent[node])return node;
-        return parent[node]=findPar(parent[node]);
-    }
-
-    void uniteByRank(int x, int y){
-        int parent_x=findPar(x), parent_y=findPar(y);
-        if(parent_x == parent_y)return;
-
-        if(rank[parent_x]>rank[parent_y])parent[parent_y]=parent_x;
-        else if(rank[parent_x]<rank[parent_y])parent[parent_x]=parent_y;
-        else parent[parent_y]=parent_x, rank[parent_x]++;
-    }
-
-    void uniteBySize(int x, int y){
-        int parent_x=findPar(x), parent_y=findPar(y);
-        if(parent_x == parent_y)return;
-
-        if(size[parent_x]>size[parent_y])parent[parent_y]=parent_x, size[parent_x]+=size[parent_y];
-        else if(size[parent_x]<size[parent_y])parent[parent_x]=parent_y, size[parent_y]+=size[parent_x];
-        else parent[parent_y]=parent_x , size[parent_x]+=size[parent_y];
-    }
-
-};
-
 class Solution {
 public:
-
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet ds(n);
-        for(auto & edge:connections){
-            ds.uniteByRank(edge[0],edge[1]);
-        }
-        int components=0;
-        for(int i=0;i<n;i++){
-            if(ds.parent[i]==i)components++;
-        }
         if(connections.size()<n-1)return -1;
+
+
+        vector<vector<int>> adj(n);
+        vector<bool>vis(n,false);
+        int components=0;
+
+
+        for(auto &connection:connections){
+            int u=connection[0],v=connection[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+
+        function<void(int)> dfs=[&](int i){
+            vis[i]=true;
+            for(auto v:adj[i])if(!vis[v])dfs(v);
+        };
+
+        for(int i=0;i<adj.size();i++){
+            if(!vis[i]){
+                components++;
+                dfs(i);
+            }
+        }
+
         return components-1;
     }
 };

58019cba399dbc677f510befe7910542328581d0
Vipul Bhardwaj <97458500+VIPUL-BHARDWAJ-279@users.noreply.github.com>
2024-10-23 15:16:09 +0530

    Time: 3 ms (97.06%) | Memory: 18.2 MB (93.71%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
+int n;
     void dfs(int rootNode, vector<vector<int>> & graph, vector<bool> & vis){
         vis[rootNode]=true;
-        for(int i=0;i<graph[rootNode].size();i++)if(graph[rootNode][i]==1 && vis[i]==false)dfs(i,graph,vis);
+        for(int i=0;i<n;i++)if(graph[rootNode][i]==1 && vis[i]==false)dfs(i,graph,vis);
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-        int n=isConnected.size(),provinceCnt=0;
+        n=isConnected.size();
+        int provinceCnt=0;
         vector<bool> vis(n,false);
-        queue<int> q;
+        
         for(int node=0;node<n;node++){
             if(vis[node]==false){
                 provinceCnt++;
